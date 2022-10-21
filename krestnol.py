46c40e177ba1d3c4b1fe043dab64f14ea261e767
TheGrayd <116295833+TheGrayd@users.noreply.github.com>
2022-10-21 11:59:57 +0300

    Update krestnol.py

--- krestnol.py
+++ krestnol.py
@@ -2,12 +2,6 @@
 fild = [1, 2, 3,
         4, 5, 6,
         7, 8, 9]
-player_1 = [0, 1, 2,
-            3, 4, 5,
-            6, 7, 8]
-player_2 = [0, 1, 2,
-            3, 4, 5,
-            6, 7, 8]
 
 print("Ввидите имя:")
 name_player_1 = input()
@@ -26,13 +20,83 @@ def print_fild():
     print(fild[6], end = " ")
     print(fild[7], end = " ")
     print(fild[8])
-print_fild()
 for i in range(len(fild)):
+    print_fild()
     symbol_p1 = "X"
     print("Ходит", name_player_1)
     turn = int(input()) - 1
-    player_1[turn] = 1
+    fild[turn] = symbol_p1
+
+    if fild[0] == symbol_p1 and fild[1] == symbol_p1 and fild[2] == symbol_p1:
+        print(name_player_1, "Победил")
+        break
+    if fild[3] == symbol_p1 and fild[4] == symbol_p1 and fild[5] == symbol_p1:
+        print(name_player_1, "Победил")
+        break
+    if fild[6] == symbol_p1 and fild[7] == symbol_p1 and fild[8] == symbol_p1:
+        print(name_player_1, "Победил")
+        break
+    if fild[0] == symbol_p1 and fild[3] == symbol_p1 and fild[6] == symbol_p1:
+        print(name_player_1, "Победил")
+        break
+    if fild[1] == symbol_p1 and fild[4] == symbol_p1 and fild[7] == symbol_p1:
+        print(name_player_1, "Победил")
+        break
+    if fild[2] == symbol_p1 and fild[5] == symbol_p1 and fild[8] == symbol_p1:
+        print(name_player_1, "Победил")
+        break
+    if fild[0] == symbol_p1 and fild[4] == symbol_p1 and fild[8] == symbol_p1:
+        print(name_player_1, "Победил")
+        break
+    if fild[2] == symbol_p1 and fild[4] == symbol_p1 and fild[6] == symbol_p1:
+        print(name_player_1, "Победил")
+        break
+    count = 0
+    for i in fild:
+       if type(i) == int:
+        count += 1
+
+    if count == 0:
+        print('Ничья')
+        break
+    print_fild()
     symbol_p2 = "O"
+    print("Ходит", name_player_2)
+    turn = int(input()) - 1
+    fild[turn] = symbol_p2
+    if fild[0] == symbol_p2 and fild[1] == symbol_p2 and fild[2] == symbol_p2:
+        print(name_player_2, "Победил")
+        break
+    if fild[3] == symbol_p1 and fild[4] == symbol_p1 and fild[5] == symbol_p1:
+        print(name_player_2, "Победил")
+        break
+    if fild[6] == symbol_p1 and fild[7] == symbol_p1 and fild[8] == symbol_p1:
+        print(name_player_2, "Победил")
+        break
+    if fild[0] == symbol_p1 and fild[3] == symbol_p1 and fild[6] == symbol_p1:
+        print(name_player_2, "Победил")
+        break
+    if fild[1] == symbol_p1 and fild[4] == symbol_p1 and fild[7] == symbol_p1:
+        print(name_player_2, "Победил")
+        break
+    if fild[2] == symbol_p1 and fild[5] == symbol_p1 and fild[8] == symbol_p1:
+        print(name_player_2, "Победил")
+        break
+    if fild[0] == symbol_p1 and fild[4] == symbol_p1 and fild[8] == symbol_p1:
+        print(name_player_2, "Победил")
+        break
+    if fild[2] == symbol_p1 and fild[4] == symbol_p1 and fild[6] == symbol_p1:
+        print(name_player_2, "Победил")
+        break
+
+    count = 0
+    for i in fild:
+        if type(i) == int:
+            count += 1
+
+    if count == 0:
+        print('Ничья')
+
     print("Ходит", name_player_2)
     turn = int(input()) - 1
     player_2[turn] = 2
